6019d322891430c9be555d07cfabe0d663e9c038
Chukwuebuka Stanley <azukachukwuebuka07@gmail.com>
2024-05-04 03:17:56 +0100

    home page mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
+  content: ["./public/*.html"],
   theme: {
     extend: {
       colors: {
         primary: "#fe8101",
+        ash: "#1A1717",
+      },
+      fontFamily: {
+        title: ["Segoe UI", "DM Sans"],
+        body: ["DM Sans", "sans-serif"],
       },
     },
   },
